868fc225e7c69ba582d0b72dcbc6035c2c643ecc
Vinay Chutake <vnyctk26@gmail.com>
2017-05-12 14:17:31 +0530

    project list style changes

--- neo_portfolio/portfolio/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from portfolio.views import IndexView, ProjectDetailsView
-
-urlpatterns = [
-    url(r'^$', IndexView.as_view(), name='index'),
-    url(r'^projects/(?P<pid>[0-9]+)/$', ProjectDetailsView.as_view(), name='project_details'),
-]
